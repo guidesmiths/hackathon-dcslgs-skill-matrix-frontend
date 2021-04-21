9f8af0e181a20ba8c5cfb6e4fabf50e48fda4962
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-04-21 18:07:11 +0200

    feat: temporary skills list

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,18 +1,28 @@
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import Input from '../../app/commons/Input/Input';
 import Select from '../../app/commons/Select/Select';
 import HomePageStyled from './HomePage.styled';
+import { selectAllSkills } from '../../redux/skills/skillsSlice';
 
-export const HomePage = () => (
-  <HomePageStyled
-    data-cy="hola"
-  >
-    <SearchBar />
-    <div>
-      <Input/>
-      <Select/>
-    </div>
-  </HomePageStyled>
-);
+export const HomePage = () => {
+  const skills = useSelector(selectAllSkills);
+  return (
+    <HomePageStyled
+      data-cy="hola"
+    >
+      <SearchBar />
+      <div>
+        <Input/>
+        <Select/>
+      </div>
+      {
+        skills.length > 0 ? (skills.map(skill =>
+          (<p key={skill.id}>{skill.name}</p>),
+        )) : (<p>Skill Store empty</p>)
+      }
+    </HomePageStyled>
+  );
+};
