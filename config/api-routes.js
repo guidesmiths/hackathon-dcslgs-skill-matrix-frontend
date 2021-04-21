72a1fc1c716b2e4e114979783e0fa06a2e220acb
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-04-21 11:35:02 +0200

    feat: skill slice fetch data

--- config/api-routes.js
+++ config/api-routes.js
@@ -2,7 +2,7 @@ module.exports = {
   api: {
     skills: {
       fetchSkills: {
-        url: `${process.env.SKILL_MATRIX_BACKEND}/api/v1/skills`,
+        url: `${process.env.SKILL_MATRIX_BACKEND}/api/v1/skills/catalog`,
         method: 'get',
       },
     },
