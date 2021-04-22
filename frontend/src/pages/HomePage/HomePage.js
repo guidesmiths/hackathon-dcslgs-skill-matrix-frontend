fdee7bbb50fd249277b255d01a54bf3fd3b2967c
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-04-22 16:05:44 +0200

    feat: clean code homepage component

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,16 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import Input from '../../app/commons/Input/Input';
 import Select from '../../app/commons/Select/Select';
 import HomePageStyled from './HomePage.styled';
-import { selectAllSkills, fetchSkillsAsync } from '../../redux/skills/skillsSlice';
+import { fetchSkillsAsync } from '../../redux/skills/skillsSlice';
 
 export const HomePage = () => {
   const dispatch = useDispatch();
-  const skills = useSelector(selectAllSkills);
-  const skillsStatus = useSelector(state => state.skills.status);
 
   useEffect(() => {
     dispatch(fetchSkillsAsync());
@@ -22,14 +20,9 @@ export const HomePage = () => {
     >
       <SearchBar />
       <div>
-        <Input skills={skills}/>
+        <Input/>
         <Select/>
       </div>
-      {
-        skillsStatus === 'succeded' ? (skills.map(skill =>
-          (<p key={skill.id}>{skill.name}</p>
-          ))) : (<p>Skill Store empty</p>)
-      }
     </HomePageStyled>
   );
 };
