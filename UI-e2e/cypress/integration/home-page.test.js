c567d391cde2738b8b822c4ada7b300f601cd3e5
David Yusta <david.yusta@guidesmiths.com>
2021-05-01 20:25:56 +0200

    test: add test for answer list component

--- UI-e2e/cypress/integration/home-page.test.js
+++ UI-e2e/cypress/integration/home-page.test.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 describe('Home page', () => {
   beforeEach(() => {
-    cy.fetchSkills();
+    cy.init();
   });
 
-  it('should render home page"', () => {
-    cy.visit('/');
-  });
+  // it.only('should render home page"', () => {
+  //   cy.visit('/');
+  // });
 
   it('should redirect to home page if route doesn\'t exist', () => {
     cy.visit('/no-exist');
@@ -14,8 +14,46 @@ describe('Home page', () => {
   });
 
   it('should find React option when filling in skill input', () => {
+    cy.visit('/');
     cy.get('input').last().type('re');
     cy.get('option').contains('React');
     cy.get('select').select('3');
+  });
+
+  describe('For the anwser list', () => {
+    it('should render the correct number of list element when visiting the page', () => {
+      cy.visit('/');
+      cy.get('[data-cy^="answer-list-element-"]').should('have.length', 2);
+    });
+
+    it('should rende the correct number of skill for a given list element', () => {
+      cy.visit('/');
+      cy.get('[data-cy="answer-list-element-1"]').within(() => {
+        cy.get('[data-cy="skill-list"] > div').should('have.length', 3);
+      });
+    });
+
+    it('should not display the skill list of an element when first rendering the AnswerListElement\'s', () => {
+      cy.visit('/');
+      cy.get('[data-cy="answer-list-element-1"]').within(() => {
+        cy.get('[data-cy="skill-list"]').should('have.css', 'display', 'none');
+      });
+    });
+
+    it('should display the skill list of an element when clicking the button', () => {
+      cy.visit('/');
+      cy.get('[data-cy="answer-list-element-1"]').within(() => {
+        cy.get('[data-cy="list-element-header"] > button').click();
+        cy.get('[data-cy="skill-list"]').should('have.css', 'display', 'block');
+      });
+    });
+
+    it('should hide the skill list of an element after clicking the button when it was displayed previously', () => {
+      cy.visit('/');
+      cy.get('[data-cy="answer-list-element-1"]').within(() => {
+        cy.get('[data-cy="list-element-header"] > button').click().click();
+        cy.get('[data-cy="skill-list"]').should('have.css', 'display', 'none');
+      });
+    });
   });
 });
