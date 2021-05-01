3ffe60900a2532c50481fe0e7f7a90113d4825cf
David Yusta <david.yusta@guidesmiths.com>
2021-05-01 20:20:31 +0200

    feat: add answers store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import skillsReducer from '../redux/skills/skillsSlice';
+import answersReducer from '../redux/answers/answersSlice';
 
 export default configureStore({
   reducer: {
     skills: skillsReducer,
+    answers: answersReducer,
   },
 });
