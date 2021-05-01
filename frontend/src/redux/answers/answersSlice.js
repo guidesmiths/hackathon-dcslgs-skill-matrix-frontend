07b3c4a1d543dce6bebdbb31c75da1a81959d59e
David Yusta <david.yusta@guidesmiths.com>
2021-05-01 20:20:09 +0200

    feat: add redux slice for answers

---
+++ frontend/src/redux/answers/answersSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  value: [],
+  status: 'idle',
+};
+
+export const fetchAnswersAsync = createAsyncThunk(
+  'answers/fetchAnswers',
+  async () => {
+    const response = await axios.get('/ui/answers');
+    return response.data;
+  },
+);
+
+export const answersSlice = createSlice({
+  name: 'answers',
+  initialState,
+  reducers: {
+    answerAdded: (state, action) => {
+      state.value = [...state.value, ...action.payload];
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchAnswersAsync.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAnswersAsync.fulfilled, (state, action) => {
+        state.status = 'succeded';
+        state.value = [...state.value, ...action.payload];
+      });
+  },
+});
+
+export const { answerAdded } = answersSlice.actions;
+
+// Selectors
+export const selectAllAnswers = state => state.answers.value;
+
+// export const selectAnswerById = (state, answerId) => state.answers.find(answers => answers.id === answerId);
+
+export default answersSlice.reducer;
