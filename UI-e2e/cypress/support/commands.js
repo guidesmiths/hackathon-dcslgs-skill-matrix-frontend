1c32ffd76275d5f08777ef695d6f7b74741af3be
David Yusta <david.yusta@guidesmiths.com>
2021-05-01 20:25:25 +0200

    test: wrap cypress commands into init

--- UI-e2e/cypress/support/commands.js
+++ UI-e2e/cypress/support/commands.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
-Cypress.Commands.add('fetchSkills', () => {
-  cy.visit('/');
+Cypress.Commands.add('init', () => {
   cy.server();
+  cy.visit('/');
   cy.route({
     url: '/ui/skills/catalog',
     method: 'get',
     response: 'fixture:skills.json',
   }).as('getAllSkills');
-  cy.wait('@getAllSkills');
+  cy.route({
+    url: '/ui/answers',
+    method: 'get',
+    response: 'fixture:answers.json',
+  }).as('getAllAnswers');
+  cy.wait(['@getAllSkills', '@getAllAnswers']);
 });
