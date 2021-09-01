49df1ce12fba587cc63d7f28b8b47e701c321ea8
Yordan Krushkov <Yordan.Krushkov@DCSLSoftware.com>
2021-09-01 16:41:13 +0100

    fix: suggestion-pop-fomr
    -removed test from the admin-page test file

--- UI-e2e/cypress/integration/user-page.test.js
+++ UI-e2e/cypress/integration/user-page.test.js
@@ -41,26 +41,3 @@ describe('User page', () => {
     cy.contains('I\'m a level 4').should('exist');
   });
 });
-
-describe('Suggestion form', () => {
-  it('should open and close suggestion modal form', () => {
-    cy.get('[data-cy^="icon-add"]').click();
-    cy.get('[data-cy^="modal"] > [data-cy^="suggestion-form"]').should('be.visible');
-    cy.contains('Cancel').click();
-    cy.get('[data-cy^="modal"]').should('be.not.visible');
-  });
-
-  it('should be able to select type and type siggestion', () => {
-    cy.get('[data-cy^="icon-add"]').click();
-    cy.get('#type').select('Skill');
-    cy.get('textarea').type('We can add some communications skills');
-    cy.contains('Cancel').click();
-  });
-
-  it('should be able to send a siggestion', () => {
-    cy.get('[data-cy^="icon-add"]').click();
-    cy.get('#type').select('Skill');
-    cy.get('textarea').type('We can add some communications skills');
-    cy.contains('Send').click();
-  });
-});
