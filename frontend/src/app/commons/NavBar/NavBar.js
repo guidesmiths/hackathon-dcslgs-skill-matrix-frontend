65cc059c2a28a39fce220cc3a0932739b39508f1
Yordan Krushkov <Yordan.Krushkov@DCSLSoftware.com>
2021-09-21 19:09:14 +0100

    feat: styled navigation
    Styled navigation,
    Added Assets folder
    Added Theme file

--- frontend/src/app/commons/NavBar/NavBar.js
+++ frontend/src/app/commons/NavBar/NavBar.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
-import { NavBarTop, NavBarLink } from './NavBar.styled';
+import { NavBarTop, NavStyled, NavBarLink, LogoWrapper, UserWrapperStyled, StyledIcon } from './NavBar.styled';
+import logo from '../../../Assets/Images/Logo_DCSLGuideSmiths.png';
 
 const NavBar = () => (
   <Fragment>
     <NavBarTop>
-      <NavBarLink to="/">Directory</NavBarLink>
-      <NavBarLink to="/profile">Personal Skill Matrix</NavBarLink>
+      <NavStyled>
+        <LogoWrapper>
+          <img src={logo}/>
+          <NavBarLink to="/" activeClassName="selected">Directory</NavBarLink>
+        </LogoWrapper>
+        <NavBarLink to="/profile" activeClassName="selected">Skill Matrix</NavBarLink>
+        <NavBarLink to="/404">Personal Skill Matrix</NavBarLink>
+      </NavStyled>
+      <UserWrapperStyled>
+        <StyledIcon icon={'face'} />
+        <NavBarLink to="/" activeClassName="selected">test@email.com</NavBarLink>
+      </UserWrapperStyled>
     </NavBarTop>
   </Fragment>
 );
 
 export default NavBar;
+
