e8ce52d9c634c8a6b8a30235d0b900749852c354
Yordan Krushkov <Yordan.Krushkov@DCSLSoftware.com>
2021-10-26 09:43:05 +0100

    fix: merged conflicts
    added media queries
    fixed merge conflicts

--- frontend/src/app/commons/ScrollWrapper/ScrollWrapper.styled.js
+++ frontend/src/app/commons/ScrollWrapper/ScrollWrapper.styled.js
@@ -7,4 +7,14 @@ export const UserRowWrapper = styled.div`
   &::-webkit-scrollbar{
     display:none;
   }
+
+  @media (max-height: 1160px) {
+    max-height: ${props => props.height - 5}vh;
+  }
+  @media (max-height: 1000px) {
+    max-height: ${props => props.height - 10}vh;
+  }
+  @media (max-height: 900px) {
+    max-height: ${props => props.height - 15}vh;
+  }
 `;
