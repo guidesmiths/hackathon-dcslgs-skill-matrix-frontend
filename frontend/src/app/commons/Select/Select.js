7c350b697161ddfa4c46345e2845898bab6ba689
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-11-18 17:55:30 +0100

    fix: select disabled when empty skill search bar

--- frontend/src/app/commons/Select/Select.js
+++ frontend/src/app/commons/Select/Select.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { SelectStyled, OptionStyled } from './Select.styled';
 
-const Select = ({ options, onChange, selected }) => (
-  <SelectStyled id="skill" name="skill" value={selected} onChange={onChange}>
+const Select = ({ options, onChange, selected, disabled }) => (
+  <SelectStyled disabled={disabled} id="skill" name="skill" value={selected} onChange={onChange}>
     {options.map((option, index) => (
       <OptionStyled key={index} value={option.value}>
         {option.value}
@@ -14,11 +14,13 @@ const Select = ({ options, onChange, selected }) => (
 
 Select.propTypes = {
   onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.boolean,
   options: PropTypes.array,
   selected: PropTypes.number,
 };
 
 Select.defaultProps = {
+  disabled: false,
   options: [],
   selected: 1,
 };
