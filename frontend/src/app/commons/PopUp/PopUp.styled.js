6c065ac7a60833701b7e01b76324c5fbba40ec68
Yordan Krushkov <Yordan.Krushkov@DCSLSoftware.com>
2022-01-17 07:12:23 +0000

    Merged PR 41428: fix: new pop up style
    - [x] Created new pop up style
    ![popup.png](https://dev.azure.com/dcslsoftwareltd/1b5d230c-65af-4baf-b375-86998d72572c/_apis/git/repositories/0226497e-0ebd-4618-9369-b3dd00574dce/pullRequests/41428/attachments/popup.png)
    
    Related work items: #131406

--- frontend/src/app/commons/PopUp/PopUp.styled.js
+++ frontend/src/app/commons/PopUp/PopUp.styled.js
@@ -15,23 +15,28 @@ const OverlayStyled = styled.div`
 const PopUpStyled = styled.div`
   position: fixed;
   bottom: 80px;
-  right:40px;
+  right:0;
+  left: 0;
+  margin: auto;
+  width: fit-content;
   max-width: 600px;
   background-color: ${props => (props.isSuccess ? '#73D13D' : '#C5292A')};
   z-index: 2;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
 `;
 
 const PopUpStyledTitleWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding: 24px;
+  justify-content: center;
+  padding: 10px;
 `;
 
 const PopUpStyledTitle = styled.h5`
   max-width: 424px;
-  font-weight: bold;
-  font-size: 24px;
+  font-weight: 500;
+  font-size: 12px;
   line-height: 32px;
   color: white;
   color: ${props => (props.isSuccess ? 'black' : 'white')};
