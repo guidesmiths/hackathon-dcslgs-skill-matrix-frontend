c687d2279b1ea5d4755d45febd5dc18ef00b05a2
Jennifer Goijman <jennifer.goijman@dcsl.com>
2022-06-07 14:40:10 +0000

    Merged PR 47279: fix: get 404 with route /
    fix: get 404 with route /
    
    Related work items: #148378

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { NavBar } from './app/commons/NavBar';
 import { fetchUserInfoAsync, selectUserData } from './redux/user/userSlice';
-import { HOME_ROUTE, LOGIN_ROUTE, USER_ROUTE, PAGE404_ROUTE, DIRECTORY_ROUTE, COUNTRY_ROUTE } from './constants/routes';
+import { HOME_ROUTE, LOGIN_ROUTE, USER_ROUTE, ADMIN_ROUTE, PAGE404_ROUTE, DIRECTORY_ROUTE, COUNTRY_ROUTE } from './constants/routes';
 import { HomePage } from './pages/HomePage';
 import { HomeUserPage } from './pages/HomeUserPage';
 import { AdminPage } from './pages/AdminPage';
@@ -47,8 +47,8 @@ const AppRouter = ({ environment }) => {
       <Switch>
         <NotLoggedRoute exact component={LoginPage} path={LOGIN_ROUTE} />
         <PrivateRoute exact component={SelectCountry} path={COUNTRY_ROUTE} />
-        <PrivateRoute exact component={UserPage} path={[USER_ROUTE, '/profile/ecosystem/', '/profile/ecosystem/:id']} />
-        {!userView && <PrivateRoute exact component={AdminPage} path={[HOME_ROUTE, '/ecosystem/', '/ecosystem/:id']} />}
+        <PrivateRoute exact component={UserPage} path={[HOME_ROUTE, USER_ROUTE, `${USER_ROUTE}/ecosystem/`, `${USER_ROUTE}/ecosystem/:id`]} />
+        {!userView && <PrivateRoute exact component={AdminPage} path={[`${ADMIN_ROUTE}`, `${ADMIN_ROUTE}/:id`]} />}
         <PrivateRoute exact component={userView ? HomeUserPage : HomePage} path={DIRECTORY_ROUTE} />
         <Route exact component={Page404}/>
         {/* Default path for non existing pages */}
--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -2,8 +2,9 @@
 const HOME_ROUTE = '/';
 const LOGIN_ROUTE = '/login';
 const USER_ROUTE = '/profile';
+const ADMIN_ROUTE = '/ecosystem';
 const DIRECTORY_ROUTE = '/directory';
 const COUNTRY_ROUTE = '/country';
 const PAGE404_ROUTE = '/404';
 
-export { HOME_ROUTE, USER_ROUTE, DIRECTORY_ROUTE, LOGIN_ROUTE, PAGE404_ROUTE, COUNTRY_ROUTE };
+export { HOME_ROUTE, USER_ROUTE, ADMIN_ROUTE, DIRECTORY_ROUTE, LOGIN_ROUTE, PAGE404_ROUTE, COUNTRY_ROUTE };
